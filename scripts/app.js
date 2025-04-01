f8729dcd43ce3dc94b28f955759b69d4090d0780
samuel-cibi <samuelcibi@gmail.com>
2025-04-02 00:04:37 +0530

    Update and rename javascripts.txt to app.js

---
+++ scripts/app.js
@@ -0,0 +1,125 @@
+document.getElementById('ABOUT-US').addEventListener("click", doSomething)
+function doSomething() {
+    alert("This website was designed by Rajkrishna T R and Samuel Cibi.\n\nFor more details, contact us at lapzone@gmail.com")
+};
+
+
+document.getElementById("online-stores").addEventListener("click", store)
+function store(
+
+) {
+    window.open("https://www.amazon.com", "_blank");
+};
+
+document.getElementById("email").addEventListener("click", email)
+function email(
+
+) {
+    window.open("https://www.gmail.com", "_blank");
+};
+
+
+document.getElementById("lap1").addEventListener("click", link1)
+function link1(
+
+) {
+    window.open("https://www.amazon.in/HP-i5-12450H-15-6-inch-Response-fa1373TX/dp/B0D2DDJ6PJ/", "_blank");
+};
+
+document.getElementById("lap2").addEventListener("click", link2)
+function link2(
+
+) {
+    window.open("https://www.amazon.in/Acer-i5-13420H-Graphics-Display-ANV15-51/dp/B0CHJJZ9G8", "_blank");
+};
+
+document.getElementById("lap3").addEventListener("click", link3)
+function link3(
+
+) {
+    window.open("https://www.amazon.in/MSI-Windows-GeForce-RTX4050-B8VE-884IN/dp/B0BV1GBXKP", "_blank");
+};
+
+document.getElementById("lap4").addEventListener("click", link4)
+function link4(
+
+) {
+    window.open("https://www.amazon.in/ASUS-Gaming-GeForce-Battery-FA506NCR-HN075WS/dp/B0D8LG5P8T", "_blank");
+};
+
+document.getElementById("lap5").addEventListener("click", link5)
+function link5(
+
+) {
+    window.open("https://www.amazon.in/ASUS-Battery-i9-13980HX-Windows-G614JV-N4141WS/dp/B0C4TVFFT6", "_blank");
+};
+
+document.getElementById("lap6").addEventListener("click", link6)
+function link6(
+
+) {
+    window.open("https://www.amazon.in/ASUS-Processor-Integrated-Graphics-UX3405CA-PZ164WS/dp/B0DSHWSDH4", "_blank");
+};
+
+document.getElementById("lap7").addEventListener("click", link7)
+function link7(
+
+) {
+    window.open("https://www.amazon.in/Acer-Predator-Processor-Windows-PHN16-72/dp/B0CXPW7BRZ", "_blank");
+};
+
+document.getElementById("lap8").addEventListener("click", link8)
+function link8(
+
+) {
+    window.open("https://www.amazon.in/Apple-2024-MacBook-Laptop-10%E2%80%91core/dp/B0DLJ82ZMT/", "_blank");
+};
+
+document.getElementById("lap9").addEventListener("click", link9)
+function link9(
+
+) {
+    window.open("https://www.amazon.in/HP-Transcend-14-Inch-48-120Hz-Fb0007Tx-Windows/dp/B0CXPY1STK/", "_blank");
+};
+
+document.getElementById("lap10").addEventListener("click", link10)
+function link10(
+
+) {
+    window.open("https://www.amazon.in/Lenovo-Snapdragon-X1P-42-100-Integrated-83HL0035IN/dp/B0DRVXHSLJ/", "_blank");
+};
+
+document.getElementById("lap11").addEventListener("click", link11)
+function link11(
+
+) {
+    window.open("https://www.amazon.in/Acer-Processor-3050-6GB-39-624cm-AL15G-53/dp/B0DXVGVNKN?th=1", "_blank");
+};
+
+document.getElementById("lap12").addEventListener("click", link12)
+function link12(
+
+) {
+    window.open("https://www.amazon.in/Dell-Inspiron-i5-1334U-Processor-39-62cm/dp/B0D2Y1BLDT", "_blank");
+};
+
+document.getElementById("lap13").addEventListener("click", link13)
+function link13(
+
+) {
+    window.open("https://www.amazon.in/2022-Apple-MacBook-Laptop-chip/dp/B0B3C9ZHJZ/", "_blank");
+};
+
+document.getElementById("lap14").addEventListener("click", link14)
+function link14(
+
+) {
+    window.open("https://www.amazon.in/ASUS-Vivobook-i7-13620H-Windows-X1502VA-BQ838WS/dp/B0DYDZQYKP/", "_blank");
+};
+
+document.getElementById("lap15").addEventListener("click", link15)
+function link15(
+
+) {
+    window.open("https://www.amazon.in/Lenovo-V15-Lifetime-Validity-Warranty/dp/B0CL7CMTXS/", "_blank");
+};
